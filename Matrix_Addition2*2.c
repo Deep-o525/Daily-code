101ea8c9344bf1603187f9bb15c38518bb4f1d38
Deep-Chauhan <deepchauhan0426@gmail.com>
2025-08-27 19:25:51 +0530

    Create Matrix_Addition2*2.c

---
+++ Matrix_Addition2*2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][2], b[2][2], sum[2][2];
+    int i, j;
+
+    printf("Enter first 2*2 matrix:\n");
+    for(i=0; i<2, j++);{
+    scanf("%d", &a[i][j]);
+    }
+}
+
+printf("Enter second 2*2 maatrix:\n");
+for(1=0; j<2; j++) {
+    for(j=0,j<2;j++) {
+    scanf("%d", &b[i][j]);
+    }
+}
+
+for(i=0; i<2; i++) {
+    for(j=0; j<2;  j++) {
+        sum[i][j] =a[i][j] + b[i][j];
+    }
+}
+
+printf("Resultant Matrix:\n");
+for(i=0; i<2; i++) {
+    for(j=0; j<2; j++) {
+    printf("%d", sum[i][j]);
+    }
+ printf("/n");
+}
+return 0;
